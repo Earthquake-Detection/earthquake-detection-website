5cb683d4b58961c665f9783b093bc2eb29fe3429
Yeabseera Abebe <yeabebe41@gmail.com>
2026-05-14 01:36:18 +0300

    Implemente a new Flask application in app.py for predicting earthquake

---
+++ backend/ai/app.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import tensorflow as tf
+import numpy as np
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+model = tf.keras.models.load_model("earthquake_model.h5")
+
+scaler = joblib.load("scaler.pkl")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    data = request.json
+
+    sample = pd.DataFrame([[
+        data['lat'],
+        data['lon'],
+        data['depth'],
+        data['mag']
+    ]], columns=['Lat', 'Lon', 'Depth', 'Mag'])
+
+    sample = scaler.transform(sample)
+
+    prediction = model.predict(sample)
+
+    predicted_class = np.argmax(prediction)
+
+    classes = [
+        "Mild Earthquake",
+        "Moderate Earthquake",
+        "Strong Earthquake",
+        "Very Strong Earthquake"
+    ]
+
+    result = classes[predicted_class]
+
+    return jsonify({
+        "prediction": result
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
